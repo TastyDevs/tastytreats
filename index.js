73b5ab59a3f3b910703e7bf6a639a4cfc7bb1d8d
furkycl <81173127+furkycl@users.noreply.github.com>
2025-08-24 12:26:18 +0000

    Deploying to gh-pages from @ TastyDevs/tastytreats@6675392f3ccfa567fff012cd9da7669a859048a3 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import"./assets/footer-C6MpvTPq.js";const I=document.querySelectorAll(".home-slides"),ie=document.querySelectorAll(".home-dot"),y=document.createElement("div");y.classList.add("home-popup");y.innerHTML=`
+import"./assets/footer-Cw73Jzmf.js";const I=document.querySelectorAll(".home-slides"),ie=document.querySelectorAll(".home-dot"),y=document.createElement("div");y.classList.add("home-popup");y.innerHTML=`
   <div class="home-popup-content">
     <span class="home-popup-close">&times;</span>
     <h2>ORDER NOW</h2>
